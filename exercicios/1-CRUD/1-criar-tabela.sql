5f35a9c991ea2842063bb38ca1a9b8860e1f497d
LuscaGerva <lucasm.gervasio@hotmail.com>
2024-12-08 15:06:57 +0000

    CriarTabela

--- exercicios/1-CRUD/1-criar-tabela.sql
+++ exercicios/1-CRUD/1-criar-tabela.sql
@@ -1,3 +1,6 @@
 -- Crie uma nova tabela no banco de dados
-SELECT *
-FROM tracks;
+CREATE TABLE salario_classe(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nivel TEXT,
+salario INTEGER
+);
--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,18 @@
 -- Insira 3 classes salariais na tabela salario_classe
-
+INSERT into salario_classe (id, salario, nivel)
+VALUES
+(1,1500,'Estagiário' ),
+(2,2500,'Assistente'),
+(3,3600,'Analista Junior'),
+(4, 4500, 'Analista Pleno'),
+(5, 5600, 'Analista Senior'),
+(6, 8000, 'Especialista');
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe(salario, nivel)
+VALUES
+(13000, 'Coordenador'),
+(15000, 'Gerente');
+
+DELETE FROM salario_classe
+WHERE id=9 OR id=10
