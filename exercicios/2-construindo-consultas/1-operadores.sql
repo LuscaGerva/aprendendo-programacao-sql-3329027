0406622aebb0e63d932fe8dbbb13a9d0085b9109
LuscaGerva <lucasm.gervasio@hotmail.com>
2024-12-08 18:56:44 +0000

    cursoSql

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,25 @@
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
-
+SELECT
+TrackId,
+UnitPrice
+FROM
+invoice_items;
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
-
+SELECT
+TrackId,
+UnitPrice,
+UnitPrice * 1.3,
+UnitPrice + 2.80
+FROM
+invoice_items
+WHERE InvoiceId = 3;
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
-
+SELECT
+TrackId,
+InvoiceId,
+UnitPrice,
+UnitPrice * 1.3,
+UnitPrice + 2.80
+FROM
+invoice_items
+WHERE InvoiceId > 20 AND InvoiceId <=30;
--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,11 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
-
+SELECT DISTINCT
+Country AS Pais,
+State AS Estado_Sigla,
+CASE
+WHEN State = 'SP' THEN 'SAO PAULO'
+WHEN State = 'RJ' THEN 'RIO DE JANEIRO'
+ELSE 'Estado Desconhecido'
+END as Estado
+FROM customers 
+WHERE Country LIKE 'Brazil';
